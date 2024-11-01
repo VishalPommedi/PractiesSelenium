40f72b30f2b44cb2f4c10ae31fd4553989f025cd
Vishal Pommedi <pommedchintu15980@gmail.com>
2024-11-01 09:15:02 +0530

    added new files for demo webshop and log files

--- Features/RandomDetails.py
+++ Features/RandomDetails.py
@@ -1,7 +1,17 @@
 import uuid
+import random
+import string
 
 def Generate_id():
     return uuid.uuid4().hex
 
+def Random_Details():
+    length = random.randint(5, 6)
 
+    FirstName = ''.join(random.choices(string.ascii_lowercase, k=length))
 
+    LastName = ''.join(random.choices(string.ascii_lowercase, k=length))
+
+    EmailID = f'{FirstName}.{LastName}@tech.com'
+
+    return FirstName, LastName, EmailID
