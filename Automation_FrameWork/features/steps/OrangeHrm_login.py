e9aa87dea9ac00ecbfdb6f7cc478c6bd3dbf7b0c
Vishal Pommedi <pommedchintu15980@gmail.com>
2025-04-16 22:30:09 +0530

    Added log functionality and save screenshot

--- Automation_FrameWork/features/steps/OrangeHrm_login.py
+++ Automation_FrameWork/features/steps/OrangeHrm_login.py
@@ -5,6 +5,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from behave import *
+import logging
+from log import LogConfigurator
+from commonsteps import *
+
+Configurator = LogConfigurator()
 
 @Given("Launch the broser")
 def orh_launch_browser(context):
@@ -18,20 +23,23 @@ def orh_launch_browser(context):
 
 @when("Open the Orange HRM webapp")
 def orh_open_url(context):
+    Configurator.beforeall(context)
     try:
+        
         wait = WebDriverWait(context.driver, 10)
         context.driver.get(orangehrm_url)
         login_text = wait.until(EC.presence_of_element_located((By.XPATH, "//h5[normalize-space()='Login']")))
 
         if login_text:
-            print("**", login_text.text)
+            logging.info(login_text.text)
         else:
-            print("Login page is not visible")
+            logging.warning("Login page is not visible")
     except Exception as e:
-        print("The error occured at orh_open_url method. the error: ", e)   
+        logging.error("The error occuredt at orh_open_url method. The error: ", e)  
 
 @then("Login with user name, and password")
 def orh_enter_uname_pass(context):
+    Configurator.beforeall(context)
     try:
         userName = context.driver.find_element(By.XPATH, username_textbox_xpath)
         password = context.driver.find_element(By.XPATH, password_textbox_xpath)
@@ -41,14 +49,17 @@ def orh_enter_uname_pass(context):
         time.sleep(1)
         password.send_keys(orh_admin_p)
         time.sleep(1)
+        logging.info(f"login with username: {orh_admin_u} and password: {orh_admin_p}")
         loging_button.click()
         
-    except Exception as e:
-        print("The error occured at orh_enter_uname_pass method. the error: ", e)    
+    except Exception as e:  
+        capture_screenshot(context) 
+        logging.error("The error occured at orh_enter_uname_pass method. the error: ", e) 
 
 @then("Naviate to the profile options and click on the About button")
 def orh_click_about_button(context):
 
+    Configurator.beforeall(context)
     try:
         wait = WebDriverWait(context.driver, 10)
 
@@ -57,7 +68,8 @@ def orh_click_about_button(context):
 
         profile_options = wait.until(EC.presence_of_all_elements_located((By.XPATH, profile_options_xpath)))
         profile_options_text = [i.text for i in profile_options]
-        print("**", profile_options_text)
+        
+        logging.info(f"Profile options: {profile_options_text}")
 
         About_button = next((i for i in profile_options if i.text == "About"), None)
 
@@ -65,15 +77,16 @@ def orh_click_about_button(context):
             About_button.click()
             
         else:
-            print("** About not found!!")
+            logging.warning("About button not found!!")
+        capture_screenshot(context)    
 
 
-    except Exception as e:
-        print("The error occured at orh_read_profile_data method. the error: ", e)    
+    except Exception as e:  
+        logging.error(f"The error occured at orh_read_profile_data method. the error: {e}")  
 
 @then("Read the user data")
 def orh_read_profile_data(context):
-
+    Configurator.beforeall(context)
     try:
 
         wait = WebDriverWait(context.driver, 10)
@@ -94,17 +107,18 @@ def orh_read_profile_data(context):
             key = text_list[i].replace(":", "")
             value = text_list[i+1]
             info_dict[key] = value
-        print("**", info_dict)
+        logging.info(f'user details: {info_dict}')
         time.sleep(2)
         close_button = context.driver.find_element(By.XPATH, aboutpage_close_button_xpath)
         close_button.click()
     except Exception as e:
         print("The error from the orh_read_profile_data. The error is: ", e)
+        logging.error(f"The error from the orh_read_profile_data. The error is: {e}")
 
 @then("Logout from page")
 def orh_logout(context):
     try:
-
+        Configurator.beforeall(context)
         wait = WebDriverWait(context.driver, 10)
         profile_button = wait.until(EC.element_to_be_clickable((By.XPATH, userprofile_xpath)))
         profile_button.click()
@@ -112,7 +126,7 @@ def orh_logout(context):
 
         profile_options = wait.until(EC.presence_of_all_elements_located((By.XPATH, profile_options_xpath)))
         profile_options_text = [i.text for i in profile_options]
-        print("**", profile_options_text)
+        logging.info(f"The profile optins: {profile_options_text}")
 
         logout_button = next((i for i in profile_options if i.text == "Logout"), None)
 
@@ -121,12 +135,12 @@ def orh_logout(context):
             time.sleep(5)
             
         else:
-            print("** unable to click on the logout button!!")
+            logging.warning("** unable to click on the logout button!!")
 
 
 
     except Exception as e:
-        print(e)
+        logging.error(f'The error occured at orh_logout method. The error: {e}')
 
 @then("close the web browser")
 def orh_close_browser(context):
